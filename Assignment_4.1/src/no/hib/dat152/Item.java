bdb69d069383e6d91826c80f4a0b7f73c53d35c5
Sindre Stokkenes <Sindre.stokkenes@gmail.com>
2016-09-14 12:13:54 +0200

    # WARNING: head commit changed in the meantime
    test

---
+++ Assignment_4.1/src/no/hib/dat152/Item.java
@@ -0,0 +1,25 @@
+package no.hib.dat152;
+
+public class Item {
+	private String name;
+	private double pris;
+	
+	Item(String n, double p){
+		this.name = n;
+		this.pris = p;
+	}
+
+	public double getPris() {
+		return pris;
+	}
+
+	public void setPris(double pris) {
+		this.pris = pris;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	
+}
---
+++ Assignment_4.1/src/no/hib/dat152/MainProgram.java
@@ -0,0 +1,51 @@
+package no.hib.dat152;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+
+public class MainProgram {
+
+	public static void main(String[] args) {
+		Locale locale = new Locale("de", "De");
+//		Locale locale = new Locale("no", "NO");
+//		Locale locale = new Locale("en", "GB");
+		
+		boolean forsett = true;
+		Store butikk = new Store(locale);
+		ResourceBundle apptexts = ResourceBundle.getBundle("apptexts", locale);
+		
+		while (forsett){
+		System.out.print(apptexts.getString("choice"));
+		Scanner input = new Scanner(System.in);
+        int i = input.nextInt();
+        	if(i == 1){
+    		System.out.print(apptexts.getString("fruits"));
+            int j = input.nextInt();
+            	if(j == 1){
+            		butikk.addItem(new Item("Apple", 400.1));
+            		System.out.println();
+            	}else if(j == 2){
+            		butikk.addItem(new Item("Banana", 360.1));
+            		System.out.println();
+            	}else if(j == 3){
+            		butikk.addItem(new Item("Pineapple", 70.1));
+            		System.out.println();
+            	}else if(j == 4){
+            		butikk.addItem(new Item("SuperPineapple", 30.1));
+            		System.out.println();
+            	}else{
+            		
+            	}
+    		
+    		
+        	}else if(i == 2){
+        	butikk.getItems();
+        	
+        	}else{
+        		forsett = false;
+        	}
+		}
+	}
+
+}
